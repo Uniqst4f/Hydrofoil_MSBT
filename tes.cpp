4bee989289752c163d89489665d4daa31e1296c2
Uniqst4f <alogohasiholan@gmail.com>
2026-03-09 20:57:29 +0700

    Kode Nama dan NRP

---
+++ tes.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int main () {
+
+  string nama;
+  int nrp;
+
+cout << "Masukan Nama :"<< "\n";
+getline(cin, nama);
+cout << "Masukan NRP :" << "\n";
+cin >> nrp;
+
+cout << nama << "\n";
+cout << nrp << "\n";
+
+
+return 0;
+
+
+}
